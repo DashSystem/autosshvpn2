6fbb37f769ced4177f656e9947e900e15aca762d
DashSystem <DashSystem@users.noreply.github.com>
2017-02-13 11:31:28 +0800

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -33,7 +33,7 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 service sshd restart
 
 # set repo
-wget http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+wget https://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 rpm -Uvh epel-release-6-8.noarch.rpm
 rpm -Uvh remi-release-6.rpm
